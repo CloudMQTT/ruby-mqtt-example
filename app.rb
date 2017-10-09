582a024d10ac5d5d84f70b4a4b960d7c7630bf5b
Magnus Landerblom <magnus@landerblom.se>
2017-10-09 14:32:38 +0200

    Dependencies update

--- app.rb
+++ app.rb
@@ -7,15 +7,16 @@ conn_opts = {
   remote_host: uri.host,
   remote_port: uri.port,
   username: uri.user,
-  password: uri.password,
+  password: uri.password
 }
+topic = uri.path[1, uri.path.length] || 'test'
 
 # Subscribe example
 Thread.new do
   MQTT::Client.connect(conn_opts) do |c|
     # The block will be called when you messages arrive to the topic
-    c.get('test') do |topic, message|
-      puts "#{topic}: #{message}"
+    c.get(topic) do |t, message|
+      puts "#{t}: #{message}"
     end
   end
 end
@@ -24,8 +25,7 @@ end
 MQTT::Client.connect(conn_opts) do |c|
   # publish a message to the topic 'test'
   loop do
-    c.publish('test', 'Hello World')
+    c.publish(topic, 'Hello World')
     sleep 1
   end
 end
-
